c21f87d353c0b4d052af16a2b0dbbbe06174c01f
Davis Yoshida <davis.yoshida@gmail.com>
2015-09-14 17:56:15 -0600

    README and fix of deadlock condition

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # qb-api
+
+Currently this API has just two calls that can be made:
+
+* /qb-api/question/<questionId>/<wordId>
+    * POST
+    * Request a word of a question
+    * Required fields:
+        * id: A valid user id (String)
+    * Updates the number of words requested for this question to be wordId if it is not higher already already.
+        * For example, if requests for words 0, 5, 1 and 1 of a particular question were sent, the user record of used words would be 0, 5, 5 and 5 respectively.
+
+    * Returns: Word  #<wordId> (indexed from 0) of question #<questionId>
+    * Example usage
+    ```sh
+        $ curl -X POST http://127.0.0.1:5000/qb-api/question/0/0 -d 'id=ident'
+        {"word":"hello"}
+    ```
+
+* /qb-api/answer/<questionId>
+    * POST
+    * Submit an answer for question #<questionId>
+    * Required fields: 
+        * id: A valid user id (String)
+        * answer: The answer for a question (String)
+    * May only be called once per user per question
+    * Example usage:
+    ```sh
+        $ curl -X POST http://127.0.0.1:5000/answer/0 -d 'id=0' -d 'answer=earth'
+        {"score"=<some score>}
+    ```
+
+Note about answers: Answers must be the title of a wikipedia page. Answers are case insensitive.
--- api.py
+++ api.py
@@ -15,7 +15,7 @@ api = Api(server, prefix="/qb-api")
 question_db = Questions()
 user_lock = Lock()
 #TODO: Replace pickling with another storage method
-if os.exists.('userdata.p'):
+if os.path.exists('userdata.p'):
     with open('userdata.p', 'rb') as f:
         users = pickle.load(f)
 else:
@@ -36,15 +36,20 @@ def validate_user_id(user_id):
 class Question(Resource):
     def post(self, question_id, word_id):
         """Handle word requests"""
+        print("Waiting for lock")
         user_lock.acquire()
         user_id = request.form['id']
+        print("Validating ID")
         validate_user_id(user_id)
         try:
             word = question_db.get(question_id, word_id)
         except IndexError:
+            user_lock.release()
             abort(400, message="Invalid question or word id.")
 
+        print("Logging query")
         users[user_id].log_query(question_id, word_id)
+        print("Logged query")
 
         user_lock.release()
         return {'word':word}
@@ -61,7 +66,9 @@ class Answer(Resource):
         try:
             success = question_db.check_answer(question_id, answer)
         except IndexError:
+            user_lock.release()
             abort(400, message="Invalid question id.")
+
         score = users[user_id].store_result(question_id, answer, success)
         user_lock.release()
         if score is not None:
