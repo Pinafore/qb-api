dadbf2b7cc0f10f90b3fdacfc897e6272919ebf7
Pedro Rodriguez <ski.rodriguez@gmail.com>
2018-05-31 08:53:10 -0700

    Update get_buzzes.py

--- web/get_buzzes.py
+++ web/get_buzzes.py
@@ -1,8 +1,9 @@
 
-from database import *
+from database import QuizBowl
 
 if __name__ == "__main__":
-    buzzes = get_buzzes()
-    for ii in sorted(buzzes):
-        val = buzzes[ii]
-        print("%i\t%i\t%i\t%s" % (ii[0], ii[1], val[0], val[1]))
+    buzzes = QuizBowl.get_buzzes()
+    with open('data/buzzes.csv', 'w') as f:
+        for ii in sorted(buzzes):
+            val = buzzes[ii]
+            print("%i\t%i\t%i\t%s" % (ii[0], ii[1], val[0], val[1]), file=f)
